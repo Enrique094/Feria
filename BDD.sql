5a97eab8e8f4d81bf6735ce2548c8fcec38333f4
Ezequiel2314 <146790254+Ezequiel06-k@users.noreply.github.com>
2025-05-23 10:30:32 -0600

    Actu de la bdd
    xd

--- BDD.sql
+++ BDD.sql
@@ -1,29 +1,23 @@
+CREATE DATABASE Gestor;
+USE Gestor;
+
+-- 1. Rango
 CREATE TABLE rango (
     id_rango INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(20) -- Ej: 'Junior', 'Senior', 'Admin'
+    nombre VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE vendedor (
-    id_vende INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(20),
-    apellido VARCHAR(20),
-    tel VARCHAR(15),
-    id_rango INT,
-    FOREIGN KEY (id_rango) REFERENCES rango(id_rango)
-);
-
-
--- Junten User y cliente en una sola tabla, y añadan un campo que indique si es cliente o vendedor.
--- Tabla de usuarios
+-- 2. Usuarios
 CREATE TABLE usuarios (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(100),
     correo VARCHAR(100) UNIQUE,
     contraseña VARCHAR(255),
     rango VARCHAR(50) DEFAULT 'User',
-    estado TINYINT(1) DEFAULT 1, 
+    estado TINYINT(1) DEFAULT 1
 );
 
+-- 3. Cliente
 CREATE TABLE cliente (
     id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(20),
@@ -34,23 +28,95 @@ CREATE TABLE cliente (
     direccion VARCHAR(50)
 );
 
-CREATE TABLE product (
+-- 4. Vendedor
+CREATE TABLE vendedor (
+    id_vende INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(20),
+    apellido VARCHAR(20),
+    tel VARCHAR(15),
+    id_rango INT,
+    id_zona INT,
+    FOREIGN KEY (id_rango) REFERENCES rango(id_rango),
+    FOREIGN KEY (id_zona) REFERENCES Zona(id_zona)
+);
+
+-- 5. Cobrador
+CREATE TABLE cobrador (
+    id_cobrador INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(20),
+    apellido VARCHAR(20),
+    tel VARCHAR(15),
+    id_rango INT,
+    id_zona INT,
+    cliente INT,
+    FOREIGN KEY (id_rango) REFERENCES rango(id_rango),
+    FOREIGN KEY (id_zona) REFERENCES Zona(id_zona),
+    FOREIGN KEY (cliente) REFERENCES cliente(id_cliente)
+);
+
+-- 6. Producto
+CREATE TABLE producto (
     id_product INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(30),
     descripcion VARCHAR(50), 
-    precio INT -- En centavos
+    precio INT NOT NULL,
+    imagen BIT(1) DEFAULT 0, 
+    stock INT DEFAULT 0, 
+    id_catego INT NOT NULL,
+    FOREIGN KEY (id_catego) REFERENCES categoria(id_categoria)
 );
 
-CREATE TABLE factura (
-    id_factura INT AUTO_INCREMENT PRIMARY KEY,
+-- 7. Categoria
+CREATE TABLE categoria (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(20),
+    descripcion VARCHAR(50)
+);
+
+-- 8. Zona
+CREATE TABLE Zona (
+    id_zona INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(20)
+);
+
+
+
+-- 9. Factura de venta
+CREATE TABLE factura_venta (
+    id_factura_venta INT AUTO_INCREMENT PRIMARY KEY,
     id_cliente INT NOT NULL,
     id_vende INT NOT NULL,
     id_product INT NOT NULL,
+    id_catego INT NOT NULL,
     fecha DATE NOT NULL,
     hora TIME NOT NULL,
-    monto INT NOT NULL, -- En centavos
-
+    monto INT NOT NULL,
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
     FOREIGN KEY (id_vende) REFERENCES vendedor(id_vende),
-    FOREIGN KEY (id_product) REFERENCES product(id_product)
+    FOREIGN KEY (id_product) REFERENCES producto(id_product),
+    FOREIGN KEY (id_catego) REFERENCES categoria(id_categoria)
+);
+
+-- 10. Factura de cobro
+CREATE TABLE factura_cobro (
+    id_factura_cobro INT AUTO_INCREMENT PRIMARY KEY,
+    id_cobrador INT NOT NULL,
+    id_zona INT NOT NULL,
+    id_cliente INT NOT NULL,
+    monto INT NOT NULL,
+    fecha DATE NOT NULL,
+    FOREIGN KEY (id_cobrador) REFERENCES cobrador(id_cobrador),
+    FOREIGN KEY (id_zona) REFERENCES Zona(id_zona),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+);
+
+CREATE TABLE contrato (
+    id_contrato INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    id_vende INT NOT NULL,
+    id_cobrador INT NOT NULL,
+    fecha DATE NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_vende) REFERENCES vendedor(id_vende),
+    FOREIGN KEY (id_cobrador) REFERENCES cobrador(id_cobrador)
 );
